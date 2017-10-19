11fc3ae7e3b1f642ff2d3ceaed77b98db0967617
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-19 01:34:33 -0400

    completed the nested each_with_index abd deleted the seat_index + 1 from the seating_plan[row] after inserting the new persons name. added prompts for sit down? and name

--- ex1.rb
+++ ex1.rb
@@ -1,4 +1,5 @@
-
+require 'pry'
+require 'pp'
 
 seating_plan = [
   [nil, "Pumpkin", nil, nil],
@@ -6,3 +7,25 @@ seating_plan = [
   ["Gismo", "Shadow", nil, nil],
   ["Smokey","Toast","Pacha","Mau"]
 ]
+
+seating_plan.each_with_index do |row, row_index|
+  # puts "#{index} index is #{row}"
+  # puts "****************************************"
+  row.each_with_index do |seat, seat_index|
+    if seat == nil
+      puts
+     p "Row # #{row_index + 1} seat #{seat_index + 1} is empty"
+     puts "Take the seat? y/n"
+     take_seat = gets.chomp.to_s
+     if take_seat == 'y'
+       puts "What is your name?"
+       name = gets.chomp.to_s
+       seats_in_row = seating_plan[row_index]
+       seats_in_row.insert(seat_index, name)
+       seats_in_row.delete_at(seat_index + 1)
+     end
+    end
+  end
+end
+
+pp seating_plan
